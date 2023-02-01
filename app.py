2f331d26791ecc4d5fc5c4411f14a0a8447a6170
AshikurRahmanMunna <ashikurrahmanmunna3@gmail.com>
2023-02-01 21:59:29 +0600

    Added packages

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-import utils
-import converters
-from utils import find_max_number
-from converters import kg_to_lbs,lbs_to_kg
+import ecommerce.shipping
 
-max_number = find_max_number([5, 8, 3, 4, 13, 6, 32, 7, 2, 4])
-lbs = kg_to_lbs(70)
-kg = lbs_to_kg(70)
-print(max_number, lbs, kg)
+ecommerce.shipping.calc_shipping()
